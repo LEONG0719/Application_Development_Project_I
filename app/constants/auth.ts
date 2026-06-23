0bb7732a123edc82f7242955efad77bb90692844
njwongzhe <ngjaawei@gmail.com>
2026-06-23 13:59:59 +0800

    Domain Restriction Activate

--- app/constants/auth.ts
+++ app/constants/auth.ts
@@ -2,5 +2,5 @@
 // This separation allows us to avoid importing client-side constants into server-side code, which can help prevent accidental exposure of sensitive information and reduce bundle size.
 
 // When you change these values, you need to ensure that you also update the corresponding values in app/constants/auth-server.ts if you try to update the domain restrictions at backend also.
-export const DOMAIN_RESTRICTION = true;
+export const DOMAIN_RESTRICTION = false;
 export const RESTRICTED_EMAIL_DOMAIN = "@johor.gov.my";
